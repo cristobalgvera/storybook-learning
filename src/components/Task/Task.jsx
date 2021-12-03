fb4840d025674458f98534594d2cc3f93d5df420
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2021-12-02 23:09:17 -0300

    Added snapshots in Jest for each Storybook component

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,9 +1,55 @@
-const Task = ({ task: { title }, onArchiveTask, onPinTask }) => {
+import PropTypes from 'prop-types';
+
+const Task = ({ task: { id, title, state }, onArchiveTask, onPinTask }) => {
   return (
-    <div data-testid="wrapper" className="list-item">
-      <input data-testid="task-input" type="text" value={title} readOnly />
+    <div className={`list-item ${state}`}>
+      <label className="checkbox">
+        <input
+          type="checkbox"
+          defaultChecked={state === 'TASK_ARCHIVED'}
+          disabled={true}
+          name="checked"
+        />
+        <span
+          className="checkbox-custom"
+          onClick={() => onArchiveTask(id)}
+          id={`archiveTask-${id}`}
+          aria-label={`archiveTask-${id}`}
+        />
+      </label>
+      <div data-testid="wrapper" className="title">
+        <input
+          data-testid="task-input"
+          type="text"
+          value={title}
+          placeholder="Input title"
+          readOnly
+        />
+      </div>
+      <div className="actions" onClick={(event) => event.stopPropagation()}>
+        {state !== 'TASK_ARCHIVED' && (
+          // eslint-disable-next-line jsx-a11y/anchor-is-valid
+          <a onClick={() => onPinTask(id)}>
+            <span
+              className={`icon-star`}
+              id={`pinTask-${id}`}
+              aria-label={`pinTask-${id}`}
+            />
+          </a>
+        )}
+      </div>
     </div>
   );
+};
+
+Task.propTypes = {
+  task: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    state: PropTypes.string.isRequired,
+  }),
+  onArchiveTask: PropTypes.func,
+  onPinTask: PropTypes.func,
 };
 
 export default Task;
